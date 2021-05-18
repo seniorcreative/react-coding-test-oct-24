79ef02d156e53f7b286fdeead72ed6faeaf70d60
Glenn McComb <glenn.mccomb@seersec.com.au>
2021-05-18 11:08:51 +1000

    adds className support to Small component

--- src/ui/library/typography/Small.js
+++ src/ui/library/typography/Small.js
@@ -1,12 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import classNames from "classnames";
 
 const propTypes = {
   children: PropTypes.node,
+  className: PropTypes.string,
 };
 
-export const Small = ({ children }) => (
-  <small className="text-xs text-gray-400">{children}</small>
+export const Small = ({ children, className }) => (
+  <small className={classNames("text-xs", "text-gray-400", className)}>
+    {children}
+  </small>
 );
 
 Small.propTypes = propTypes;
